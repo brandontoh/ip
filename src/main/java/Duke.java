f6a165de320e129d2e3613604f148ab3e825e271
brandontoh <brandontoh@yahoo.com.sg>
2020-08-22 20:50:31 +0800

    Add Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,11 +7,29 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("____________________________________________________________");
+        dottedLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        dottedLine();
+
+        while (true) {
+            System.out.println("Please enter a command:");
+
+            Scanner input = new Scanner(System.in);
+            String line = input.nextLine();
+
+            if (line.toLowerCase().equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(line);
+            }
+
+            dottedLine();
+        }
+    }
+
+    public static void dottedLine() {
         System.out.println("____________________________________________________________");
     }
 }
