be1f5ea83d6321b6b6949750e1525676cb6f26f1
brandontoh <brandontoh@yahoo.com.sg>
2020-09-12 16:18:19 +0800

    Change taskList to ArrayList

--- src/main/java/task/TaskManager.java
+++ src/main/java/task/TaskManager.java
@@ -1,38 +1,30 @@
 package task;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class TaskManager {
-    static protected Task[] taskList;
-    static protected int taskCount;
+    static protected ArrayList<Task> taskList;
     protected Task task;
 
     public TaskManager() {
-        taskList = new Task[100];
-        taskCount = 0;
-    }
-
-    public Task[] getList() {
-        return taskList;
+        taskList = new ArrayList<Task>();
     }
 
     public static int getTaskCount() {
-        return taskCount;
+        return taskList.size();
     }
 
     public Task getTask(int index) {
-        return taskList[index];
+        return taskList.get(index);
     }
 
     public void addToList(Task task) {
-        taskList[taskCount] = task;
-        taskCount++;
+        taskList.add(task);
         System.out.println("Got it. I've added this task:");
     }
 
     public void markAsCompleted(int index) {
-        Task task = taskList[index];
+        Task task = taskList.get(index);
         task.markAsCompleted();
     }
 
@@ -55,13 +47,10 @@ public class TaskManager {
     }
 
     public void deleteFromList(int index) {
-        ArrayList<Task> tempList = new ArrayList<Task>(Arrays.asList(taskList));
-        if (index <= 0 || index > taskCount) {
+        if (index <= 0 || index > getTaskCount()) {
             System.out.println("Invalid task number");
         } else {
-            tempList.remove(index - 1);
-            taskList = tempList.toArray(new Task[100]);
-            taskCount--;
+            taskList.remove(index - 1);
         }
     }
 }
